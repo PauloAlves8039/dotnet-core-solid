e0a173c53a14a1bca44a030bad683fa8e2ecb26f
PauloAlves8039 <pj38alves@gmail.com>
2022-06-21 20:39:02 -0300

    Versão inicial do projeto

---
+++ src/GerenciamentoDeDescontro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GerenciamentoDeDescontro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Gerenciamento de Desconto *****");
+
+            Console.ReadLine();
+        }
+    }
+}
